8e68059bc9149a225367751b3abbb618fa4216f8
SpacyBen <bendautil432@gmail.com>
2026-01-25 17:24:12 +0800

    update

--- step1 data structure/detectduplicate.py
+++ step1 data structure/detectduplicate.py
@@ -6,14 +6,26 @@ def data1(a1):
         if w not in seen:
             occure.append(w)
             seen.add(w)
-        elif w in duplicate:
+        if w in duplicate:
             duplicate[w] = duplicate.get(w) + 1
         else:
             duplicate[w] = duplicate.get(w,0) + 1
+    for laman in list(duplicate.keys()):
+        print(laman)
+        if duplicate[laman] <= 1:
+            del duplicate[laman]
     return duplicate, occure
-
+def data2(a2):
+    occure = {}
+    for w in a2:
+        if w in occure:
+            occure[w] = occure.get(w) + 1
+        else:
+            occure[w] = occure.get(w,0) + 1
+    return occure
 
 word = ["banana","saging","banana","apple"]
 dupe, occ = data1(word)
 print(occ)
 print("Duplicates: ",dupe)
+print(data2(word))
