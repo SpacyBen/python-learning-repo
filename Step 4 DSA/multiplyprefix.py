bed4588a4a51c3bcaa36972d5ed63321c2507746
SpacyBen <bendautil432@gmail.com>
2026-02-09 23:34:55 +0800

    another prefix multiplying exept value of index

---
+++ Step 4 DSA/multiplyprefix.py
@@ -0,0 +1,34 @@
+def pref1(arr):
+    n = len(arr)
+    left = [0] * n
+    right = [0] * n
+    output = []
+    for i in range(len(arr)):
+        if i == 0:
+            left[0] = arr[i]
+        else:
+            left[i] = left[i-1] * arr[i-1]
+   
+    for i in range(len(arr)-1,-1,-1):
+        print(i)
+        if i == (len(arr)-1):
+            right[i] = arr[i]
+        elif i == (len(arr)-2):
+            right[i] = arr[i+1]
+        else:
+            right[i] = right[i+1] * arr[i+1]
+    
+    for i in range(len(arr)):
+        output.append(left[i] * right[i])
+    return left,right,output
+
+def pref2(arr):
+    output = []
+    for i in range(len(arr)):
+        print(i)
+        
+
+
+arr = [1,2,3,4,5]
+print(pref1(arr))
+
--- Step 4 DSA/slicingwindowandrange.py
+++ Step 4 DSA/slicingwindowandrange.py
@@ -6,7 +6,7 @@ def subarrays_with_sum(arr, target):
     # build prefix sum
     prefix = [0] * (n + 1)
     for i in range(n):
-        prefix[i + 1] = prefix[i] + arr[i]
+        prefix[i + 1] = prefix[i] + arr[i] #[0][1][3][6][8][13]
 
     # check all subarrays (O(n^2) approach)
     for start in range(n):
@@ -16,10 +16,55 @@ def subarrays_with_sum(arr, target):
                 result.append(arr[start:end + 1])
 
     return count, result
+# #start
+# 0 to 5 - end 0 to 5
+# prefix[0+1]=1 - prefix[start]=0  == target . no
+# prefix[1+1]=3 - prefix[start]=0  == target . no
+# prefix[2+1]=6 - prefix[start]=0  == target . no
+# prefix[3+1]=4 - prefix[start]=0  == target . no
 
+# start = 1
+# 0 to 5 - end 0 to 5
+# prefix[0+1]=1 - prefix[start]=1  == target . no
+# prefix[1+1]=3 - prefix[start]=1  == target . no
+# prefix[2+1]=6 - prefix[start]=1  == target . yes
+# prefix[3+1]=8 - prefix[start]=1  == target . no
+# prefix[4+1]=13 - prefix[start]=1  == target . no
+# start = 2
+# 0 to 5 - end 0 to 5
+# prefix[0+1]=1 - prefix[start]=3  == target . no
+# prefix[1+1]=3 - prefix[start]=3  == target . no
+# prefix[2+1]=6 - prefix[start]=3  == target . no
+# prefix[3+1]=8 - prefix[start]=3  == target . yes
+# prefix[4+1]=13 - prefix[start]=3  == target . 
+# start = 3
+# 0 to 5 - end 0 to 5
+# prefix[0+1]=1 - prefix[start]=6  == target . no
+# prefix[1+1]=3 - prefix[start]=6  == target . no
+# prefix[2+1]=6 - prefix[start]=6  == target . no
+# prefix[3+1]=8 - prefix[start]=6  == target . no
+# prefix[4+1]=13 - prefix[start]=6 == target . no
+# start = 4
+# 0 to 5 - end 0 to 5
+# prefix[0+1]=1 - prefix[start]=8  == target . no
+# prefix[1+1]=3 - prefix[start]=8  == target . no
+# prefix[2+1]=6 - prefix[start]=8  == target . no
+# prefix[3+1]=8 - prefix[start]=8  == target . no
+# prefix[4+1]=13 - prefix[start]=8 == target . yes
+# start = 5
+# 0 to 5 - end 0 to 5
+# prefix[0+1]=1 - prefix[start]=13  == target . no
+# prefix[1+1]=3 - prefix[start]=13  == target . no
+# prefix[2+1]=6 - prefix[start]=13  == target . no
+# prefix[3+1]=8 - prefix[start]=13  == target . no
+# prefix[4+1]=13 - prefix[start]=13 == target . 
+# prefix[5+1]= - prefix[start]=13 == target . yes
+
+# all count if continue = 3
 # Example
 arr = [1, 2, 3, 2, 5]
 target = 5
 count, subarrays = subarrays_with_sum(arr, target)
 print("Count:", count)
 print("Subarrays:", subarrays)
+ 
