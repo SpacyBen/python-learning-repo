b08f0c48232c25c2a854baba2a63bcceb32c42a7
SpacyBen <bendautil432@gmail.com>
2026-02-26 21:23:07 +0800

    more recursion prac

--- step 5 Recursion/palindrome.py
+++ step 5 Recursion/palindrome.py
@@ -1,4 +1,17 @@
 def pal(p):
-    return p == p[::-1]
+    if len(p) <= 1:
+        return True
+    if p[0] == p[-1]:
+        return pal(p[1:-1])
+    else:
+        return False
+    
+def pal2(p):
+    if len(p) <= 1:
+        return True
+    return p[0] == p[-1] and pal2(p[1:-1])
 
-print(pal("madam"))
+print(pal("madamz"))
+print(pal2("madamz"))
+pall = "madam"
+print(pall[1:-1])
